b61be2c8d9e551be4538fc8f637c16c698986d3d
Tlacaelel <tlacaelel@adgenticplatform.com>
2025-06-10 17:08:49 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://adgentic.github.io',
   base: '/adgentic.github.io',
 })
